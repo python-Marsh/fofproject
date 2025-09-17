fff8639b0ba82c9de8ed7242a61ebcf7505682f1
python-Marsh <wd97409002@gmail.com>
2025-09-18 00:05:07 +0800

    set_up_key

--- import.py
+++ import.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from openai import OpenAI
+import os
+
+
+
 
 client = OpenAI(
-  api_key="
+  api_key=os.getenv("OPENAI_API_KEY")
 )
-
 response = client.responses.create(
   model="gpt-5-nano",
   input="write a haiku about ai",
